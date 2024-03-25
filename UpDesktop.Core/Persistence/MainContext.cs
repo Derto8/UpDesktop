761e309111180dddd5607110bbdad9f1bfef3b9f
Николай Полозов <kolya.polozov@inbox.ru>
2024-03-25 12:39:24 +0500

    add MainContext file

---
+++ UpDesktop.Core/Persistence/MainContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UpDesktop.Core.Entities;
+
+namespace UpDesktop.Core.Persistence
+{
+    public class MainContext : DbContext
+    {
+        public MainContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Requests> Requests { get; set; }
+        public DbSet<History> Histories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=avikdb;Trusted_Connection=True;");
+            optionsBuilder.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+        }
+    }
+}
