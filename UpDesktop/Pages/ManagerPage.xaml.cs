5843796aa1ad8e8a8e43314af7d916aa91d49e8a
Николай Полозов <kolya.polozov@inbox.ru>
2024-03-25 13:00:25 +0500

    add Otchet page

--- UpDesktop/Pages/ManagerPage.xaml.cs
+++ UpDesktop/Pages/ManagerPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using UpDesktop.Core.Persistence;
 
 namespace UpDesktop.Pages
 {
@@ -20,9 +22,84 @@ namespace UpDesktop.Pages
     /// </summary>
     public partial class ManagerPage : Page
     {
-        public ManagerPage()
+        MainWindow mainWindow;
+        MainContext mainContext;
+        public ManagerPage(MainWindow mainWindow)
         {
             InitializeComponent();
+            this.mainWindow = mainWindow;
+            mainContext = new MainContext();
+
+            dtRequests.ItemsSource = mainContext.Users.Where(c => c.Role == "Worker").ToList();
+            combWorkers.ItemsSource = mainContext.Users.Where(c => c.Role == "Worker").Select(c => c.Login).ToList();
+            combIds.ItemsSource = mainContext.Requests.Select(c => c.Number).ToList();
+        }
+
+        private void GoEdit(object sender, RoutedEventArgs e)
+        {
+            mainWindow.OpenPage(MainWindow.pages.requests);
+        }
+
+        private async void AddWorker(object sender, RoutedEventArgs e)
+        {
+            if (combIds.Text != "")
+            {
+                if (combWorkers.Text != "")
+                {
+                    var request = await mainContext.Requests.FirstOrDefaultAsync(c => c.Number == combIds.Text);
+
+                    if (request != null)
+                    {
+                        var user = await mainContext.Users.FirstOrDefaultAsync(c => c.Login == combWorkers.Text);
+                        if (user != null)
+                        {
+                            user.RequestId = request.Id;
+                            mainContext.Update(user);
+                            await mainContext.SaveChangesAsync();
+                            dtRequests.ItemsSource = mainContext.Users.Where(c => c.Role == "Worker").ToList();
+                            MessageBox.Show("Был назначен работник");
+                        }
+                    }
+                }
+                else MessageBox.Show("Выберите работника");
+            }
+            else MessageBox.Show("Выберите заявку");
+        }
+
+        private void Otchet(object sender, RoutedEventArgs e)
+        {
+            mainWindow.OpenPage(MainWindow.pages.statistics);
+        }
+
+        private void Exit(object sender, RoutedEventArgs e)
+        {
+            mainWindow.OpenPage(MainWindow.pages.auth);
+        }
+
+        private async void ChangeTime(object sender, RoutedEventArgs e)
+        {
+            if (combIds.Text != "")
+            {
+                if (tbData.Text != "")
+                {
+                    var date = DateTime.TryParse(tbData.Text, out DateTime time) ? time : DateTime.MinValue;
+                    if (date == DateTime.MinValue)
+                    {
+                        MessageBox.Show("Введите правильно дату!");
+                        return;
+                    }
+
+                    var requestId = mainContext.Requests.FirstOrDefault(c => c.Number == combIds.Text);
+                    requestId.Date = date;
+
+                    mainContext.Update(requestId);
+                    await mainContext.SaveChangesAsync();
+
+                    MessageBox.Show("Время заявки было изменено!");
+                }
+                else MessageBox.Show("Введите дату!");
+            }
+            else MessageBox.Show("Выберите заявку");
         }
     }
 }
---
+++ UpDesktop/Pages/Otchet.xaml.cs
@@ -0,0 +1,96 @@
+﻿using iTextSharp.text.pdf;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using UpDesktop.Core.Persistence;
+
+namespace UpDesktop.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Otchet.xaml
+    /// </summary>
+    public partial class Otchet : Page
+    {
+        MainWindow mainWindow;
+        MainContext mainContext;
+        public Otchet(MainWindow mainWindow)
+        {
+            InitializeComponent();
+            this.mainWindow = mainWindow;
+
+            mainContext = new MainContext();
+            combIds.ItemsSource = mainContext.Requests.Select(c => c.Number).ToList();
+        }
+
+        private void Add(object sender, RoutedEventArgs e)
+        {
+            if (combIds.Text != "")
+            {
+                if (tbTime.Text != "")
+                {
+                    if (tbMaterials.Text != "")
+                    {
+                        if (tbCost.Text != "")
+                        {
+                            if (tbProblem.Text != "")
+                            {
+                                if (tbHelp.Text != "")
+                                {
+                                    var cost = double.TryParse(tbCost.Text, out double coost) ? coost : 0;
+                                    if (coost == 0)
+                                    {
+                                        MessageBox.Show("Введите цену цифрами!");
+                                        return;
+                                    }
+
+                                    var ttime = TimeOnly.TryParse(tbTime.Text, out TimeOnly time) ? time : TimeOnly.MinValue;
+                                    if (time == TimeOnly.MinValue)
+                                    {
+                                        MessageBox.Show("Введите правильно затраченное время!");
+                                        return;
+                                    }
+
+                                    iTextSharp.text.Document document = new iTextSharp.text.Document();
+                                    PdfWriter.GetInstance(document, new FileStream("Otchet.pdf", FileMode.Create));
+                                    document.Open();
+                                    document.Add(new iTextSharp.text.Paragraph($"Application report: {combIds.Text}"));
+                                    document.Add(new iTextSharp.text.Paragraph($"Time spent: {tbTime.Text}"));
+                                    document.Add(new iTextSharp.text.Paragraph($"Materials used: {tbMaterials.Text}"));
+                                    document.Add(new iTextSharp.text.Paragraph($"Cost: {tbCost.Text}"));
+                                    document.Add(new iTextSharp.text.Paragraph($"Cause of malfunction: {tbProblem.Text}"));
+                                    document.Add(new iTextSharp.text.Paragraph($"Assistance provided: {tbHelp.Text}"));
+                                    document.Close();
+
+                                    MessageBox.Show("Отчёт был сгенерирован");
+                                }
+                                else MessageBox.Show("Введите оказанную помощь");
+                            }
+                            else MessageBox.Show("Введите причину неисправности");
+                        }
+                        else MessageBox.Show("Введите стоимость");
+                    }
+                    else MessageBox.Show("Введите затраченные материалы");
+                }
+                else MessageBox.Show("Введите затраченное время");
+            }
+            else MessageBox.Show("Выберите номер заявки");
+        }
+
+        private void Auth(object sender, RoutedEventArgs e)
+        {
+            mainWindow.OpenPage(MainWindow.pages.workerReq);
+        }
+    }
+}
